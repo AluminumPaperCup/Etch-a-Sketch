f7a8329a1a2e1a8a33b1f9d07910b30306b93a90
Brandon <104651603+AluminumPaperCup@users.noreply.github.com>
2024-08-02 15:10:34 -0400

    FIX add period to queryselector property

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Variable to that contains container for 16x16 grid
-const motherDiv = document.querySelector("bigBox");
+const motherDiv = document.querySelector(".bigBox");
 
 // Function for 16x16 grid that appends to motherDiv
 for (let i = 1; i < 257; i++) {
