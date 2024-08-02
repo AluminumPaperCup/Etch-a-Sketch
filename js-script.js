369e3ba45b357e76bfb434545de32b0ad74e07df
Brandon <104651603+AluminumPaperCup@users.noreply.github.com>
2024-08-02 14:55:24 -0400

    ADD loop function that creates divs within the container div, bigBox

--- js-script.js
+++ js-script.js
@@ -1,2 +1,8 @@
 // Variable to that contains container for 16x16 grid
 const motherDiv = document.querySelector("bigBox");
+
+// Function for 16x16 grid that appends to motherDiv
+for (let i = 1; i < 257; i++) {
+    const div = document.createElement("div");
+    motherDiv.appendChild(div);
+}
