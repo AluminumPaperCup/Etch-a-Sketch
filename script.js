88585d667efa5e8334ce922477fcd6418b62465c
Brandon <104651603+AluminumPaperCup@users.noreply.github.com>
2024-08-07 19:54:00 -0400

    ADD new class to div element created by for loop. This gives the ability for CSS to handle this element better.

--- script.js
+++ script.js
@@ -4,5 +4,7 @@ const motherDiv = document.querySelector(".bigBox");
 // Function for 16x16 grid that appends to motherDiv
 for (let i = 1; i <= 256; i++) {
     const div = document.createElement("div");
+    // Add CSS class for div for precise handling
+    div.classList.add('babyDiv');
     motherDiv.appendChild(div);
-}
+};
