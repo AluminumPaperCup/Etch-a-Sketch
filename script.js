47b7f65a292f8fd783dfa536e7d12d13b488c1b1
Brandon <104651603+AluminumPaperCup@users.noreply.github.com>
2024-08-07 17:18:06 -0400

    CHANGE middle i expression in for loop to exact value of 16x16

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const motherDiv = document.querySelector(".bigBox");
 
 // Function for 16x16 grid that appends to motherDiv
-for (let i = 1; i < 257; i++) {
+for (let i = 1; i <= 256; i++) {
     const div = document.createElement("div");
     motherDiv.appendChild(div);
 }
