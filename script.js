f716c8c8240842c2055a1d6dac6a36c0a2a462e9
Brandon <104651603+AluminumPaperCup@users.noreply.github.com>
2024-08-09 18:59:00 -0400

    ADD variable and statement that takes prompt input and considers if the input is valid to avoid nonsense error inputs.

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ const gridButton = document.querySelector("#gridButton");
 gridButton.addEventListener("click", (event) => {
     const grid = prompt("Please enter a number to generate new grid (e.g. 100 = 100 x 100 grid square)", "");
 
+    // Ensure grid is a valid number
+    const gridSize = parseInt(grid);
+    if (isNaN(gridSize) || gridSize <= 0 || gridSize > 100) {
+        alert("Invalid number. You are an infidel");
+        return;
+    };
+
 motherDiv.addEventListener("mouseover", (event) => {
     
     if (event.target.classList.contains('babyDiv')) {
